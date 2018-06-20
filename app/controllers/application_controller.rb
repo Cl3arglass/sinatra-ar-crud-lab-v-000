ad9144ae4f2c22b3184ab9c16097bdaa86f5ab14
Holden Altaffer <haltaffer@gmail.com>
2018-06-20 06:28:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,42 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  post '/posts' do
+    @post = Post.create(params[:post])
+    erb :show
+
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    # name = params[:name]
+    # content = params[:content]
+    # @post = Post.find(params[:id])
+    # @post.update({:name => params[:name], :content => params[:content]})
+    # @post.save
+    # @post.name = params[:name]
+    # @post.content = params[:content]
+    # erb :show
+    "#{params}"
+  end
 end
